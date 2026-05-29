87a85b7b1600cdd6f908aef7c5442a870b5df503
Bruno Altamirano <brunoaltfercorgmail.com>
2026-05-29 13:52:55 +0200

    Initial Commit

---
+++ BirdApp/BirdAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdAppApp.swift
+//  BirdApp
+//
+//  Created by Bruno Altamirano on 29/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct BirdAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
